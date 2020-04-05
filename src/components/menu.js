75c9099090e788057ed041bd3948d87d4323cbef
Egor <goro4@mail.ru>
2020-04-05 23:16:16 +0500

    доделал 2.1

---
+++ src/components/menu.js
@@ -0,0 +1,12 @@
+/**
+ *  меню
+ * @return{html} возращает разметку
+ */
+export const createSiteMenuTemplate = () => {
+  return (
+    `<nav class="trip-controls__trip-tabs  trip-tabs">
+       <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
+       <a class="trip-tabs__btn" href="#">Stats</a>
+    </nav>`
+  );
+};
